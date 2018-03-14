9a06899d27955be1bcb7588b7fc93238fabf7ea1
KeeganC <36234333+KeeganC@users.noreply.github.com>
2018-03-14 19:44:03 -0400

    Update MainWindow.xaml.cs
    updated to working draft 1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,17 +28,50 @@ namespace QuadrantQuandary
 
         private void btnGo_Click(object sender, RoutedEventArgs e)
         {
+            int xValue, yValue;
+            String strX, strY;
             string stringInput = txbInput.Text;
-            Console.WriteLine(stringInput);
+            //NIU Console.WriteLine(stringInput);
             Char charRange = '\r';
             int startNewLine = stringInput.IndexOf(charRange);
             // To find last occurrence of the character use: int endIndex = stringInput.LastIndexOf(charRange);
-            Console.WriteLine(startNewLine);
-            int start2nd = stringInput.IndexOf(charRange)+2;
-            Console.WriteLine(start2nd);
+            //NIU Console.WriteLine(startNewLine);
+            int start2nd = stringInput.IndexOf(charRange) + 2;
+            //NIU Console.WriteLine(start2nd);
             if (start2nd == 5)
             {
                 Console.WriteLine("You did a good");
+            }
+            //getting first coord
+            strX = txbInput.Text.Substring(0, txbInput.Text.IndexOf("\r"));
+            //Tryparse changes str to int
+            int.TryParse(strX, out xValue);
+            //second coord
+            strY = txbInput.Text.Substring(txbInput.Text.IndexOf("\r") + 2, txbInput.Text.Length - txbInput.Text.IndexOf("\r") - 2);
+            int.TryParse(strY, out yValue);
+
+            //now we check if they are positive or not
+            if (xValue > 0)
+            {
+                if (yValue > 0)
+                {
+                    MessageBox.Show("Quadrant 1");
+                }
+                else
+                {
+                    MessageBox.Show("Quadrant 4");
+                }
+            }
+            else
+            {
+                if (yValue > 0)
+                {
+                    MessageBox.Show("Quadrant 2");
+                }
+                else
+                {
+                    MessageBox.Show("Quadrant 3");
+                }
             }
         }
     }
